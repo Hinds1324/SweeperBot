591bd2b2fe881630d29b3bbc5146e2b8fe4a1fff
Sini <hinds13245@gmail.com>
2021-10-16 00:26:46 +0100

    Refactored code for recording user command inputs

--- sweeperbot/user_inputs.py
+++ sweeperbot/user_inputs.py
@@ -15,3 +15,10 @@ def set_last_user_input(user, key, value):
     input_dict = last_user_inputs.get(user) or {}
     input_dict[key] = value
     last_user_inputs[user] = input_dict
+
+
+def resolve_input(user, key, value, default_value):
+    value = value or get_last_user_input(user, key) or default_value
+    set_last_user_input(user, key, value)
+
+    return value
