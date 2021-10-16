8ebc9f55b06351d147bcd533d399b179d201c523
Sini <hinds13245@gmail.com>
2021-10-16 23:01:39 +0100

    + Documentation in bot.py
    The added documentation explains how commands should be defined in the bot.py file.

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
+#######################################################################################################################
+# Setup
+
 import os
 import sweeperbot
 from discord.ext import commands
 from dotenv import load_dotenv
 
 
-# Read bot token from .env file
+# Read bot token from .env file.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Create an instance of the bot client
+# Create an instance of the bot client.
 bot = commands.Bot(command_prefix="sw?")
 
-
+# Print a message to the terminal as soon as the bot is live.
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -19,6 +22,10 @@ async def on_ready():
 
 #######################################################################################################################
 # Commands
+
+# This is where we define all the commands that can be executed by the bot. If a command is not defined here then
+# the bot has no way of knowing about it. However, actual implementation of these commands should be separated into
+# a separate module in the sweeperbot package.
 
 # Note about default command arguments: Some command arguments will be set to have a default value of None. When this
 # is the case, this doesn't mean that the default value of the argument should be thought of as None. Rather, this
